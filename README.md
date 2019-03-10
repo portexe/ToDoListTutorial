bb80417ed09005b95a9a48e7b5e8dbf20be6debc
Zack <zackazt44@gmail.com>
2019-03-09 23:40:16 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# ToDoListTutorial
+## To-Do List Tutorial
+
+---
+
+This is the finished code for [this tutorial](https://youtu.be/hqfI7Fxnhgs) on my YouTube channel.
+
+Please feel free to add on to this code and enhance it any way you like.
+
+#### Links
+
+[Website](http://www.portexe.com/)
+[Patreon](https://www.patreon.com/portexe)
+[Twitter](https://twitter.com/portexe)
---
+++ script.js
@@ -0,0 +1,57 @@
+const storageKey = "tutorial_todos";
+
+const convertStringToObj = (str) => JSON.parse(str) || [];
+
+const convertObjToString = (obj) => JSON.stringify(obj) || '';
+
+const getTodos = () => convertStringToObj(localStorage.getItem(storageKey));
+
+const addTodo = (todo) => localStorage.setItem(storageKey, convertObjToString([...getTodos(), todo]));
+
+const deleteTodo = (todo) => localStorage.setItem(storageKey, convertObjToString(getTodos().filter(_todo => _todo !== todo)));
+
+const buildTodoEl = (todo) => {
+    const el = document.createElement("li");
+    el.classList.add("list-group-item");
+    el.innerText = todo;
+    return el;
+}
+
+const appendLiToDom = (el) => document.getElementById("todo-list-container").appendChild(el);
+
+const clearTodoListDisplay = () => document.getElementById("todo-list-container").innerHTML = "";
+
+const clearInput = () => document.getElementById("new-todo-input").value = "";
+
+const displayTodos = () => {
+    clearInput();
+    clearTodoListDisplay();
+    getTodos().forEach(_todo => appendLiToDom(buildTodoEl(_todo)));
+    initClickListeners();
+}
+const initClickListeners = () => {
+    Array.from(document.getElementsByClassName("list-group-item")).forEach(_item => {
+        _item.addEventListener("click", ($event) => {
+            const todo = $event.target.innerText;
+            if(window.confirm("Have you completed this task: " + todo)) {
+                deleteTodo(todo);
+                displayTodos();
+            }
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => displayTodos());
+
+document.getElementById("submit-new-todo-btn").addEventListener("click", ($event) => {
+    const newTodoInput = document.getElementById("new-todo-input");
+    if(newTodoInput.value) {
+        addTodo(newTodoInput.value.trim());
+        displayTodos();
+    }
+});
+
+// document.getElementById("reset-storage-btn").addEventListener("click", ($event) => {
+//     localStorage.removeItem(storageKey);
+//     displayTodos();
+// });
